443c5662de35f6ec529b8f4c090d8f6352b5d73e
Shumate <Zachary.Shumate@kindred.com>
2018-08-27 12:18:10 -0400

    created custom pipe

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -1,11 +1,12 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { IProduct } from "./product";
 
 @Component({
     selector: 'pm-products',
-    templateUrl: './product-list.component.html'
+    templateUrl: './product-list.component.html',
+    styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
     pageTitle: string = 'Product List'
     imageWidth: number = 50;
     imageMargin: number = 2;
@@ -66,5 +67,9 @@ export class ProductListComponent {
 
         toggleImage(): void {
           this.showImage = !this.showImage;
+        }
+
+        ngOnInit(): void {
+          console.log('In OnInit')
         }
 }
