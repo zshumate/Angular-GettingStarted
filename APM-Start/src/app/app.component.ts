d3c28fff6d92fd48144912baf6d4cff264786ea0
Shumate <Zachary.Shumate@kindred.com>
2018-08-27 11:24:53 -0400

    Now displays list of items and has toggleable images

--- APM-Start/src/app/app.component.ts
+++ APM-Start/src/app/app.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
   selector: 'pm-root',
   template: `
     <div><h1>{{pageTitle}}</h1>
-      <pm-products><pm-products>
+      <pm-products></pm-products>
     </div>`
 })
 export class AppComponent {
