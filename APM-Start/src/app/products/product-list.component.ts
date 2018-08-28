3f80ee9200c68252403781f0456dd52840c25647
Shumate <Zachary.Shumate@kindred.com>
2018-08-28 10:49:08 -0400

    Finished part 10: retrieve product data using http request insted of hard coded in service

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -12,6 +12,7 @@ export class ProductListComponent implements OnInit {
   imageWidth: number = 50;
   imageMargin: number = 2;
   showImage: boolean = false;
+  errorMessage: string;
   
   _listFilter: string;
   get listFilter(): string {
@@ -44,7 +45,12 @@ export class ProductListComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.products = this.productService.getProducts();
-    this.filteredProducts = this.products;
+    this.productService.getProducts().subscribe(
+      products => {
+        this.products = products;
+        this.filteredProducts = this.products;
+      },
+      error => this.errorMessage = <any>error
+    );
   }
 }
